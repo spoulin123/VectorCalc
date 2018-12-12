9e6b2a3478b90fe31b5c0490bf96801b7d28b22c
Brett Sullivan <bsulliva0@gmail.com>
2018-12-12 10:08:30 -0500

    Random

--- VectorCalc/ViewController.swift
+++ VectorCalc/ViewController.swift
@@ -4,7 +4,11 @@
 //
 //  Created by Samuel Poulin on 12/10/18.
 //  Copyright © 2018 SamPoulin. All rights reserved.
+<<<<<<< HEAD
 //  Test123 TESTTESTTEST Another Test
+=======
+//  This is a test
+>>>>>>> brett
 //
 
 import UIKit
